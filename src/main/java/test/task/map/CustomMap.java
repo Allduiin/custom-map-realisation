b6ccfe4df1f2336e016fe8fcca67bf448b91529c
Allduiin <vovapro01@gmail.com>
2021-07-20 13:40:08 +0300

    Deleted unnecessary public modifiers at interface

--- src/main/java/test/task/map/CustomMap.java
+++ src/main/java/test/task/map/CustomMap.java
@@ -3,15 +3,15 @@ package test.task.map;
 import java.util.function.Consumer;
 
 public interface CustomMap<V> {
-    public void put(Long key, V value);
+    void put(Long key, V value);
 
-    public V getValue(Long key);
+    V getValue(Long key);
 
-    public int getSize();
+    int getSize();
 
-    public void clear();
+    void clear();
 
-    public boolean isEmpty();
+    boolean isEmpty();
 
-    public void forEach(Consumer<? super V> cons);
+    void forEach(Consumer<? super V> cons);
 }
